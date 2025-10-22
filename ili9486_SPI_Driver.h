7ebcca20ba5f253e9dd95da42cdb5b9fb496264f
22brappleye <67557647+voxyI@users.noreply.github.com>
2025-10-22 13:24:46 -0400

    Create ili9486_SPI_Driver.h
    header file for ili9486 driver

---
+++ ili9486_SPI_Driver.h
@@ -0,0 +1,37 @@
+/*
+ * ili9486.h
+ *
+ *  Created on: Oct 10, 2025
+ *      Author: benra
+ */
+
+#ifndef INC_ILI9486_H_
+#define INC_ILI9486_H_
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#define DC_PIN				SPI1_DC_Pin
+#define DC_PORT 			SPI1_DC_GPIO_Port
+
+#define CS_PIN				SPI1_NSS_Pin
+#define CS_PORT				SPI1_NSS_GPIO_Port
+
+#define RESET_PIN			SPI1_RESET_Pin
+#define RESET_PORT			SPI1_RESET_GPIO_Port
+
+#define LED_PIN				LED_H_Pin //This and the associated function can be left out if not needed
+#define LED_PORT			LED_H_GPIO_Port
+
+#define TIMEOUT				1000
+
+void LCD_Init();
+void send_data(uint8_t data);
+void send_command(uint8_t command);
+void send_frame_buffer(uint8_t *frame, uint16_t xStart, uint16_t yStart, uint16_t xEnd, uint16_t yEnd);
+void set_region(uint16_t xStart, uint16_t yStart, uint16_t xEnd, uint16_t yEnd);
+void set_pixel(uint16_t x, uint16_t y, uint16_t color);
+void format_frame_buffer(uint8_t *buffer, uint16_t length);
+void set_backlight(bool state);
+
+#endif /* INC_ILI9486_H_ */
